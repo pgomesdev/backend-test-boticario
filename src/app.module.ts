fa10ef97d7209ee7f3f16a2b40ddac14c351552e
pgomesdev <pgomesdev@gmail.com>
2021-01-16 16:13:46 -0300

    feat(app): added PurchaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { PurchasesModule } from './modules/purchases/purchases.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './modules/users/users.module';
     MongooseModule.forRoot(process.env.MONGODB_STRING_CONN, { useCreateIndex: true }),
     AuthModule,
     UsersModule,
+    PurchasesModule,
   ],
   controllers: [],
   providers: [],
