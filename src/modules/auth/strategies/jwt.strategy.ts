666c7e5ed00c6833ff81610ddd20b25894f5042f
pgomesdev <pgomesdev@gmail.com>
2021-01-16 16:12:26 -0300

    feat(auth): implemented jwt strategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/modules/users/users.service';
+
+type Payload = {
+  sub: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.APP_SECRET_KEY,
+    })
+  }
+
+  async validate(payload: Payload) {
+    const { sub } = payload;
+
+    return this.usersService.findById(sub);
+  }
+}
