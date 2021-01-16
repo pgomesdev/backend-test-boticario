086dbd7b96ffa42080298b516b80d170b2cbdd22
pgomesdev <pgomesdev@gmail.com>
2021-01-16 14:39:35 -0300

    feat(app): added a new mongoose option and the new auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_STRING_CONN),
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(process.env.MONGODB_STRING_CONN, { useCreateIndex: true }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
