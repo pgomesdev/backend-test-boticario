7df6e8dc97ea7d9428dc70e35dcb84e9e87ac3a8
pgomesdev <pgomesdev@gmail.com>
2021-01-18 19:03:22 -0300

    feat(users): added cpf validator

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './schemas/user.schema';
-import * as bcrypt from 'bcrypt';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -21,18 +21,18 @@ export class UsersService {
       });
 
     if (!!userWithEmailOrCpfExists) {
-      throw new BadRequestException({ error: 'A user with email or cpf already exists.' });
+      throw new BadRequestException({ error: 'A user with email or CPF already exists.' });
     }
 
-    // TODO: validate if CPF is valid
+    const isAValidCpf = this.validateCpf(cpf);
 
-    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS, 10));
+    if (!isAValidCpf) {
+      throw new BadRequestException({ error: 'The CPF provided is invalid.' });
+    }
 
-    const newUser = await this.userModel.create({ name, cpf, email, password: hashedPassword });
+    const hashedPassword = await hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS, 10));
 
-    await newUser.save();
-
-    return newUser;
+    return this.userModel.create({ name, cpf, email, password: hashedPassword });
   }
 
   async findByEmail(email: string): Promise<User | undefined> {
@@ -41,5 +41,27 @@ export class UsersService {
 
   async findById(id: string): Promise<User | undefined> {
     return this.userModel.findById(id).lean();
+  }
+
+  validateCpf(cpf: string): boolean {
+    const digits = cpf.split('');
+
+    if (digits.every(digit => digit === digits[0])) return false;
+
+    const firstDigitCalculationSum = digits.slice(0, 9).reduce((acc, digit, index) => +digit * (10 - index) + acc, 0);
+    const firstIdentifierDigit = +digits[9];
+    const firstModResult = (firstDigitCalculationSum * 10) % 11;
+    const normalizedFirstModResult = firstModResult < 10 ? firstModResult : 0;
+
+    if (normalizedFirstModResult !== firstIdentifierDigit) return false;
+
+    const secondDigitCalculationSum = digits.slice(0, 10).reduce((acc, digit, index) => +digit * (11 - index) + acc, 0);
+    const secondIdentifierDigit = +digits[10];
+    const secondModResult = (secondDigitCalculationSum * 10) % 11;
+    const normalizedSecondModResult = secondModResult < 10 ? secondModResult : 0;
+
+    if (normalizedSecondModResult !== secondIdentifierDigit) return false;
+
+    return true;
   }
 }
